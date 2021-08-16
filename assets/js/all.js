4925b1c7023f115d3dc5bfc1b45081eee2643a3f
Peggy Tsai <pinklover1671291991@gmail.com>
2021-08-16 17:45:02 +0800

    Update 2021-08-16T09:44:59.393Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,14 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
