b78c4eaadea78e6f65aff5dcb1c692d204fd082c
“PeggyTsai” <pinklover1671291991@gmail.com>
2021-12-01 23:28:05 +0800

    update index.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,12 +3,7 @@ $(function() {
 });
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-    spaceBetween: 30,
-    breakpoints: {
-      768:{
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-    }
+  slidesPerView: 3,
+  spaceBetween: 30,
+
 });
