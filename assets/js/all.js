f5ac9d4f75fa8bb887912f3d6c0862bd99b88ee0
“PeggyTsai” <pinklover1671291991@gmail.com>
2021-12-17 21:43:49 +0800

    Update 2021-12-17T13:43:43.739Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,7 +4,24 @@ $(function () {
   console.log('Hello Bootstrap5');
 });
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2.3,
+      spaceBetween: 25
+    },
+    // when window width is >= 996px
+    996: {
+      slidesPerView: 2.7,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
